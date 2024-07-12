855b3ae4f86bf9befae45a3e90dced66f5ec5614
Shivanshu Kharia <Shivanshukharia89@gmail.com>
2024-07-12 18:59:35 +0530

    Integrate Bootstrap in the application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+//= require rails-ujs
+//= require activestorage
+//= require jquery
+//= require bootstrap
+//= require_tree .
+//= require turbo
+
+import { Turbo } from "@hotwired/turbo-rails"
+import "bootstrap"
